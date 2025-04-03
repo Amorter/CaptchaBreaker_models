d8e2b63c30e5e429216bf7bd06e8182436d9b880
秦诗染 <1145252643@qq.com>
2025-04-04 01:31:15 +0800

    init

--- my_datasets.py
+++ my_datasets.py
@@ -4,4 +4,4 @@ datasets_dir = os.path.join(os.path.dirname(__file__), 'datasets')
 
 def get_dataset(dataset_name: str):
     if dataset_name == 'captcha_chinese_click_1':
-        return MsDataset.load('Amorter/captcha_chinese_click_1', subset_name='default', split='train', cache_dir=datasets_dir)
+        return MsDataset.load('Amorter/captcha_chinese_click_1', subset_name='default', split='train', cache_dir=os.path.join(datasets_dir, "captcha_chinese_click_1"))
