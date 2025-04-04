baa84e686c169c7665adb092e1103f1c6eafdd97
秦诗染 <1145252643@qq.com>
2025-04-05 00:57:45 +0800

    init

--- my_datasets.py
+++ my_datasets.py
@@ -13,5 +13,6 @@ name = modelscope_name if hub == Hubs.modelscope else name
 name = huggingface_name if hub == Hubs.huggingface else name
 
 def get_dataset(dataset_name: str):
+    os.makedirs(datasets_dir, exist_ok=True)
     if dataset_name == 'captcha_chinese_click_1':
         return MsDataset.load(name + '/captcha_chinese_click_1', subset_name='default', split='train', cache_dir=os.path.join(datasets_dir, "captcha_chinese_click_1"), hub=hub)
