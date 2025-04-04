52954c4340eece5b829c15e92ecfaf8a9b600a0a
秦诗染 <1145252643@qq.com>
2025-04-05 00:52:27 +0800

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# CaptchaBreaker_models
+这个仓库包含了CaptchaBreaker项目所使用的模型、训练数据和训练方法.
+
+
+
+## 引用声明
+
+本项目引用了如下开源项目，在此对原作者表示感谢。
+
+### ultralytics/yolov11
+- **作者**: Ultralytics LLC
+- **项目名称**: YOLOv11
+- **协议**: GNU Affero General Public License v3.0 (AGPL-3.0)
+- **仓库地址**: https://github.com/ultralytics
+
+### pytorch
+- **作者**: Facebook's AI Research team (FAIR)及其他贡献者
+- **项目名称**: PyTorch
+- **协议**: BSD-style license，具体请参阅 [PyTorch LICENSE](https://github.com/pytorch/pytorch/blob/main/LICENSE)
+- **仓库地址**: https://github.com/pytorch/pytorch
--- my_datasets.py
+++ my_datasets.py
@@ -1,7 +1,17 @@
 from modelscope import MsDataset
+from modelscope.utils.constant import Hubs
 import os
+
+
 datasets_dir = os.path.join(os.path.dirname(__file__), 'datasets')
+modelscope_name = "Amorter"
+huggingface_name = "Amort"
+name = None
+
+hub = Hubs.modelscope
+name = modelscope_name if hub == Hubs.modelscope else name
+name = huggingface_name if hub == Hubs.huggingface else name
 
 def get_dataset(dataset_name: str):
     if dataset_name == 'captcha_chinese_click_1':
-        return MsDataset.load('Amorter/captcha_chinese_click_1', subset_name='default', split='train', cache_dir=os.path.join(datasets_dir, "captcha_chinese_click_1"))
+        return MsDataset.load(name + '/captcha_chinese_click_1', subset_name='default', split='train', cache_dir=os.path.join(datasets_dir, "captcha_chinese_click_1"), hub=hub)
