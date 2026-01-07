7dd4a5271d5d273679eba911d621cb50d3e3897c
Amoter <1145252643@qq.com>
2026-01-08 02:37:12 +0800

    修正

--- my_datasets.py
+++ my_datasets.py
@@ -4,15 +4,21 @@ import os
 
 
 datasets_dir = os.path.join(os.path.dirname(__file__), 'datasets')
-modelscope_name = "Amorter"
-huggingface_name = "Amort"
-name = None
+repo_configs = {
+    'modelscope_config':{
+        'name': 'Amorter',
+        'hub': Hubs.modelscope
+    },
+    'huggingface_config':{
+        'name': 'Amort',
+        'hub': Hubs.huggingface
+    }
+}
 
-hub = Hubs.modelscope
-name = modelscope_name if hub == Hubs.modelscope else name
-name = huggingface_name if hub == Hubs.huggingface else name
+# 配置仓库
+repo_config = repo_configs['modelscope_config']
 
 def get_dataset(dataset_name: str):
     os.makedirs(datasets_dir, exist_ok=True)
     if dataset_name == 'captcha_chinese_click_1':
-        return MsDataset.load(name + '/captcha_chinese_click_1', subset_name='default', split='train', cache_dir=os.path.join(datasets_dir, "captcha_chinese_click_1"), hub=hub)
+        return MsDataset.load(repo_config['name'] + '/captcha_chinese_click_1', subset_name='default', split='train', cache_dir=os.path.join(datasets_dir, "captcha_chinese_click_1"), hub=repo_config['hub'])
